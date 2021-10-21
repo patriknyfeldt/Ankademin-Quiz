8fccb776697780374ae6f4064cc94335de504a44
patriknyfeldt <patriknyfeldt@hotmail.com>
2021-10-21 18:59:48 +0200

    added question objects, added one question with checkboxes instead of radio buttons, made a lot of changes in html and js. added  next question function  and see result button and some more stuff

--- main.js
+++ main.js
@@ -1,54 +1,161 @@
 const listOfQuestions = [
     {
         questionnr: "Fråga 1",
-        question: "Hur många bultar finns det i Ölandsbron?",
-        answer1: "svar ett",
-        answer2: "svar två",
-        answer3: "svar tre",
-        rightAnswer: "answer1"
+        question: "Man får använda två av nedanstående vindruvor när man producerar champagne, vilka två?",
+        answer1: "Pinot noir",
+        answer2: "Sauvignon Blanc",
+        answer3: "Pinot Meunier",
+        rightAnswer: "answer1" + "answer3"
     },
     {
         questionnr:"Fråga 2",
         question: "Är fiskbullar gott?",
-        answer1: "svar fyra",
-        answer2: "svar fem",
-        answer3: "svar sex",
+        answer1: "Nej",
+        answer2: "Ja",
+        answer3: "Lite gott",
+        rightAnswer: "answer1"
+    },
+    {
+        questionnr: "Fråga 3",
+        question: "Vilken är den godaste pizzan?",
+        answer1: "Margherita",
+        answer2: "Vesuvio",
+        answer3: "Napoletana",
+        rightAnswer: "answer2"
+    },
+    {
+        questionnr: "Fråga 4",
+        question: "I vilken vinregion producerar man godast vin??",
+        answer1: "Bourgogne",
+        answer2: "Piemonte",
+        answer3: "Toscana",
+        rightAnswer: "answer2"
+    },
+    {
+        questionnr: "Fråga 5",
+        question: "Vilken spansk stad har bäst restauranger?",
+        answer1: "Barcelona",
+        answer2: "Madrid",
+        answer3: "San Sebastian",
         rightAnswer: "answer3"
-  
+    },
+    {
+        questionnr: "Fråga 6",
+        question: "Vilken pastasort är godast?",
+        answer1: "Agnolotti al Plin",
+        answer2: "Ravioli",
+        answer3: "Tortellini",
+        rightAnswer: "answer1"
+    },
+    {
+        questionnr: "Fråga 7",
+        question: "Vilken ölsort är godast?",
+        answer1: "India pale ale",
+        answer2: "Lager",
+        answer3: "Stout",
+        rightAnswer: "answer2"
+    },
+    {
+        questionnr: "Fråga 8",
+        question: "Vilken fisk är godast?",
+        answer1: "Aborre",
+        answer2: "Sjötunga",
+        answer3: "Piggvar",
+        rightAnswer: "answer3"
+    },
+    {
+        questionnr: "Fråga 9",
+        question: "Är 10 frågor fler än man kan tro?",
+        answer1: "Ja",
+        answer2: "Nej",
+        answer3: "Vet ej",
+        rightAnswer: "answer1"
+    },
+    {
+        questionnr: "Fråga 10",
+        question: "Vilket land i världen producerar godast vin?",
+        answer1: "Italien",
+        answer2: "Frankrike",
+        answer3: "Spanien",
+        rightAnswer: "answer1"
     }
 ];
-
+ 
 let body = document.querySelector("body");
+let startStopContainer = document.querySelector("#startStopContainer");
 let modeBtn = document.querySelector("#mode");
 let reloadBtn = document.querySelector("#reload");
 let startBtn = document.querySelector("#startButton");
 let answerBtn = document.querySelector("#answerButton");
+let answerContainer = document.querySelector("#answerContainer");
 let questionHeading = document.querySelector("#questionHeading");
+let radioButtonContainer = document.querySelector(".radioButtons");
 let questions = document.querySelector(".questions");
 let question = document.querySelector("#question");
 let label1 = document.querySelector("#label1");
 let label2 = document.querySelector("#label2");
 let label3 = document.querySelector("#label3");
 let optionButtons = document.querySelectorAll("[name='options']");
+let checkBoxes = document.querySelectorAll("[name='checkbox']");
+let checkBoxContainer = document.querySelector(".checkBoxContainer");
+let checkBoxLabel1 = document.querySelector("#checkBoxLabel1");
+let checkBoxLabel2 = document.querySelector("#checkBoxLabel2");
+let checkBoxLabel3 = document.querySelector("#checkBoxLabel3");
 let rightAnswers = [];
 
+
 let start = () => {
+
+    radioButtonContainer.style.display = "block";
+    answerContainer.style.display = "block";
     questions.style.display = "block";
     startBtn.style.display = "none";
+
+            radioButtonContainer.style.display = "none";
+            checkBoxContainer.style.display = "block";
+            questionHeading.innerHTML = listOfQuestions[0].questionnr;
+            question.innerHTML = listOfQuestions[0].question;
+            checkBoxLabel1.innerHTML = listOfQuestions[0].answer1;
+            checkBoxLabel2.innerHTML = listOfQuestions[0].answer2;
+            checkBoxLabel3.innerHTML = listOfQuestions[0].answer3;
+    }
+
+ let nextQuestion = () => {
         if(listOfQuestions.length === 0){
+            answerBtn.style.display = "none";
+            radioButtonContainer.style.display = "none";
             questions.style.display = "none";
             let endOfQuizText = document.createElement("h2");
-            endOfQuizText.innerHTML = `You scored ${rightAnswers.length} out of 10!`
-            document.querySelector("#startStopContainer").appendChild(endOfQuizText);
+            endOfQuizText.innerHTML = "Nu har du svarat på alla frågor! Klicka för att se ditt resultat."
+            startStopContainer.appendChild(endOfQuizText);
+            let seeResultBtn = document.createElement("button");
+            seeResultBtn.innerHTML = "klicka här för att se resultat";
+            seeResultBtn.classList.add("darklight");
+            startStopContainer.appendChild(seeResultBtn);
+            seeResultBtn.addEventListener('click', () => {
+                let scoreMsg = document.createElement("h2");
+                scoreMsg.innerHTML = `Du fick ${rightAnswers.length} rätt av 10 möjliga.`;
+                if(rightAnswers.length > 10 * 0.75){
+                    scoreMsg.style.color = "green";
+                }
+                else if(rightAnswers.length > 10 * 0.5){
+                    scoreMsg.style.color = "orange";
+                }
+                startStopContainer.appendChild(scoreMsg);
+            })
+            
         }
         else{
+        checkBoxContainer.style.display = "none";    
+        radioButtonContainer.style.display = "block";
         questionHeading.innerHTML = listOfQuestions[0].questionnr;
         question.innerHTML = listOfQuestions[0].question;
         label1.innerHTML = listOfQuestions[0].answer1;
         label2.innerHTML = listOfQuestions[0].answer2;
         label3.innerHTML = listOfQuestions[0].answer3;
         } 
-}
+    }
+
 
     
 
@@ -62,21 +169,34 @@ reloadBtn.addEventListener('click', () => {
 startBtn.addEventListener('click', start);
 
 answerBtn.addEventListener('click', () => {
+    let arrayOfCheckedBoxes = [];
+
+    if(listOfQuestions.length === 10){
+        checkBoxes.forEach((box) => {
+            if(box.checked){
+                arrayOfCheckedBoxes.push(box.value);
+                let answer = arrayOfCheckedBoxes[0] + arrayOfCheckedBoxes[1]; 
+                if(answer === listOfQuestions[0].rightAnswer){
+                    rightAnswers.push(answer);
+                }
+            }
+        })
+    }
     
     optionButtons.forEach((btn) => {
-        if(btn.checked){
+    if(btn.checked){
             if(btn.value === listOfQuestions[0].rightAnswer){
                 rightAnswers.push(btn.value);
             }
-            btn.checked === false;
+            btn.checked = false;
         }
+    
     })
+    
     listOfQuestions.shift();
-    start();
+    nextQuestion();
 
 });
-
-
 
 
 
