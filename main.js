f99ce9913436ca345bc7879fe8688738403ae9c0
patriknyfeldt <patriknyfeldt@hotmail.com>
2021-10-21 13:52:27 +0200

    added functionality for the answer button, an array for saving right answers to be able to count them, and some other stuff. Needs cleaning and more objects.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const listOfQuestions = [
         answer1: "svar ett",
         answer2: "svar två",
         answer3: "svar tre",
-        rightAnswer: "svar tre"
+        rightAnswer: "answer1"
     },
     {
         questionnr:"Fråga 2",
@@ -13,40 +13,43 @@ const listOfQuestions = [
         answer1: "svar fyra",
         answer2: "svar fem",
         answer3: "svar sex",
-        rightAnswer: "svar fyra"
+        rightAnswer: "answer3"
   
     }
 ];
-
-
 
 let body = document.querySelector("body");
 let modeBtn = document.querySelector("#mode");
 let reloadBtn = document.querySelector("#reload");
 let startBtn = document.querySelector("#startButton");
 let answerBtn = document.querySelector("#answerButton");
-console.log(answerBtn);
 let questionHeading = document.querySelector("#questionHeading");
 let questions = document.querySelector(".questions");
 let question = document.querySelector("#question");
 let label1 = document.querySelector("#label1");
 let label2 = document.querySelector("#label2");
 let label3 = document.querySelector("#label3");
-
+let optionButtons = document.querySelectorAll("[name='options']");
+let rightAnswers = [];
 
 let start = () => {
     questions.style.display = "block";
     startBtn.style.display = "none";
         if(listOfQuestions.length === 0){
-            alert ("you answered all questions!");
+            questions.style.display = "none";
+            let endOfQuizText = document.createElement("h2");
+            endOfQuizText.innerHTML = `You scored ${rightAnswers.length} out of 10!`
+            document.querySelector("#startStopContainer").appendChild(endOfQuizText);
         }
+        else{
         questionHeading.innerHTML = listOfQuestions[0].questionnr;
         question.innerHTML = listOfQuestions[0].question;
         label1.innerHTML = listOfQuestions[0].answer1;
         label2.innerHTML = listOfQuestions[0].answer2;
         label3.innerHTML = listOfQuestions[0].answer3;
-    
+        } 
 }
+
     
 
 modeBtn.addEventListener('click', () => {
@@ -55,12 +58,22 @@ modeBtn.addEventListener('click', () => {
 reloadBtn.addEventListener('click', () => {
     location.reload();
 })
-console.log(startBtn);
 
 startBtn.addEventListener('click', start);
+
 answerBtn.addEventListener('click', () => {
+    
+    optionButtons.forEach((btn) => {
+        if(btn.checked){
+            if(btn.value === listOfQuestions[0].rightAnswer){
+                rightAnswers.push(btn.value);
+            }
+            btn.checked === false;
+        }
+    })
     listOfQuestions.shift();
     start();
+
 });
 
 
